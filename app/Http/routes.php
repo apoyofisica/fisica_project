65758a5b5d2cf082bd6b7ba06cc1fc28ebed6182
raunogroch <raul.noguera.rocha@gmail.com>
2018-06-14 09:04:20 -0400

    adding new origin master

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,15 +15,3 @@ Route::get('/', function () {
     return view('frontend/index');
 });
 
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-Route::post('password/reset', 'Auth\PasswordController@postReset');
-
-
-
-Route::group(['middleware' => ['auth','docente'], 'prefix'=> 'docente'], function () {
-    Route::resource('/', 'DocenteController');
-});
